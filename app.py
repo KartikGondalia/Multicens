526754c9cce5cba205364c0996fbeea749ad4203
KartikGondalia <63635017+KartikGondalia@users.noreply.github.com>
2024-05-01 00:21:28 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -13,6 +13,7 @@ import logging
 import itertools
 import numpy as np
 import pandas as pd
+from io import StringIO
 from pathlib import Path
 from memory_profiler import profile
 from util.ranking import getRanking
@@ -260,7 +261,6 @@ def run_sample():
 # Tool v2.0 (beta)
 @app.route('/example_run/<uniqueID>', methods=['GET'])
 def example_run(uniqueID):
-    #print("In side")
     conn = sqlite3.connect('results.db')
     c = conn.cursor()
 
@@ -353,41 +353,48 @@ def send_email(to_addr, subject, body):
         print(f"Error sending email: {str(e)}")
         
 
-def store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, query_genes):
+def store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, query_genes, geneSelectionK, geneSelectionList):
     conn = sqlite3.connect('results.db')
     c = conn.cursor()
+
+    # Prepare JSON data for the list column if available
+    geneSelectionList_json = json.dumps(geneSelectionList) if geneSelectionList is not None else None
     
     c.execute("""INSERT INTO results 
-                 (uniqueID, result_data, tNames, submissionTime, expirationTime, csv_path, time_taken, measure, query_genes) 
-                 VALUES (?, ?, ?, ?, ?, NULL, NULL, ?, ?)""", 
-              (uniqueID, None, tNames, submissionTime, expirationTime, measure, json.dumps(query_genes)))
+                 (uniqueID, result_data, tNames, submissionTime, expirationTime, csv_path, time_taken, measure, query_genes, geneSelectionK, geneSelectionList) 
+                 VALUES (?, ?, ?, ?, ?, NULL, NULL, ?, ?, ?, ?)""", 
+              (uniqueID, None, tNames, submissionTime, expirationTime, measure, json.dumps(query_genes), geneSelectionK, geneSelectionList_json))
     conn.commit()
     conn.close()
 
-def update_result_in_db(uniqueID, result, time_taken, result_csv):
+def update_result_in_db(uniqueID, result, time_taken, result_csv, geneSelectionResult):
     conn = sqlite3.connect('results.db')
     c = conn.cursor()
+
+    if geneSelectionResult:
+        # If query_genes has a value, store it
+        geneSelectionResult = geneSelectionResult
+    else:
+        # If query_genes is None or empty, store None
+        geneSelectionResult = None
     
     c.execute("""UPDATE results 
-                 SET result_data = ?, time_taken = ?, csv_path = ?
+                 SET result_data = ?, time_taken = ?, csv_path = ?, geneSelectionResult=?
                  WHERE uniqueID = ?""", 
-              (json.dumps(result.values.tolist()), time_taken, result_csv, uniqueID))
+              (json.dumps(result.values.tolist()), time_taken, result_csv, geneSelectionResult, uniqueID))
     conn.commit()
     conn.close()
 
 
-
-
-
-@app.route('/get_result/<uniqueID>', methods=['GET'])
-def display_result(uniqueID):
+@app.route('/search_result/<uniqueID>', methods=['GET'])
+def search_result(uniqueID):
     conn = sqlite3.connect('results.db')
     c = conn.cursor()
     app.logger.info(f"Fetching result for uniqueID: {uniqueID}")
 
     try:
         c.execute("""SELECT result_data, tNames, submissionTime, expirationTime, csv_path, 
-                     time_taken, measure, common_genes, common_sample, query_genes, no_common_sample, no_common_gene
+                     time_taken, measure, common_genes, common_sample, query_genes, no_common_sample, no_common_gene, geneSelectionResult
                      FROM results WHERE uniqueID=?""", (uniqueID,))
         result_data = c.fetchone()
     except sqlite3.Error as e:
@@ -413,6 +420,7 @@ def display_result(uniqueID):
     query_genes = json.loads(result_data[9]) if result_data[9] else []
     no_common_sample = result_data[10]
     no_common_gene = result_data[11]
+
 
     if result_data[0] is None:
         # Ensure the format string matches your database's format
@@ -442,7 +450,126 @@ def display_result(uniqueID):
         
 
         return render_template('ranking.html', columns=columns, 
-                                rows=result_list[:2000], tNames=tNames, uniqueID=uniqueID,
+                                rows=result_list[:], tNames=tNames, uniqueID=uniqueID,
+                                submissionTime=submissionTime, expirationTime=expirationTime, 
+                                time_taken=time_taken, path=path,
+                                measure=measure, common_genes=common_genes, 
+                                common_sample=common_sample, query_genes=query_genes, 
+                                no_common_sample=no_common_sample, no_common_gene=no_common_gene, hgnc_code=hgnc_code)
+
+
+@app.route('/get_result/<uniqueID>', methods=['GET'])
+def display_result(uniqueID):
+    conn = sqlite3.connect('results.db')
+    c = conn.cursor()
+    app.logger.info(f"Fetching result for uniqueID: {uniqueID}")
+
+    try:
+        c.execute("""SELECT result_data, tNames, submissionTime, expirationTime, csv_path, 
+                     time_taken, measure, common_genes, common_sample, query_genes, no_common_sample, no_common_gene, geneSelectionK, geneSelectionList, geneSelectionResult
+                     FROM results WHERE uniqueID=?""", (uniqueID,))
+        result_data = c.fetchone()
+    except sqlite3.Error as e:
+        app.logger.error(f"Database error for uniqueID: {uniqueID} - {e}")
+        return "Database error", 500
+    finally:
+        conn.close()
+
+    if not result_data:
+        return "No result found for this uniqueID", 404
+    
+    
+    tNames = result_data[1]
+    submissionTime = result_data[2]
+    expirationTime = result_data[3]
+    path = result_data[4]
+    time_taken = result_data[5]
+    measure = result_data[6]
+    # Capitalize the first letter
+    measure = measure[0].upper() + measure[1:]
+    common_genes = json.loads(result_data[7]) if result_data[7] else []
+    common_sample = json.loads(result_data[8]) if result_data[8] else []
+    query_genes = result_data[9] if result_data[9] else None 
+    no_common_sample = result_data[10]
+    no_common_gene = result_data[11]
+    geneSelectionK = result_data[12]
+    geneSelectionList = json.loads(result_data[13]) if result_data[13] is not None else []
+    print(geneSelectionList)
+
+    if result_data[0] is None:
+        # Ensure the format string matches your database's format
+        submission_datetime_str = result_data[2]
+        # Remove the time zone information for simplicity
+        submission_datetime_str = re.sub(r' GMT[+-]\d{4} \(.+\)$', '', submission_datetime_str)
+
+        try:
+            submission_datetime = datetime.strptime(submission_datetime_str, '%a %b %d %Y %H:%M:%S')
+            # Check if more than 4 hours have elapsed since submission
+            if datetime.now() - submission_datetime > timedelta(hours=4):
+                # More than 4 hours have passed, return a timeout error
+                return "Your result calculation has exceeded the maximum time limit", 400
+        except ValueError as e:
+            # Error in parsing the submission time
+            app.logger.error(f"Error parsing submissionTime for uniqueID: {uniqueID} - {e}")
+            return "Error processing submission time", 400
+
+        # Less than 4 hours have passed, continue waiting for the result
+        return render_template('waiting.html',tNames=tNames, uniqueID=uniqueID,
+                                submissionTime=submissionTime, expirationTime=expirationTime, 
+                                time_taken=time_taken,
+                                measure=measure)
+    else:
+        columns = ['Tissue Name', 'Gene Name', 'Centrality', 'Rank']
+        result_list = json.loads(result_data[0])
+        #print(result_list)
+        
+
+        if geneSelectionK != 'Null':
+            print("No")
+            return render_template('ranking.html', columns=columns, 
+                                rows=result_list[:geneSelectionK], tNames=tNames, uniqueID=uniqueID,
+                                submissionTime=submissionTime, expirationTime=expirationTime, 
+                                time_taken=time_taken, path=path,
+                                measure=measure, common_genes=common_genes, 
+                                common_sample=common_sample, query_genes=query_genes, 
+                                no_common_sample=no_common_sample, no_common_gene=no_common_gene, hgnc_code=hgnc_code)
+        
+        elif geneSelectionList != 'Null' and geneSelectionList != 'undefined':
+            print("-------------------")
+            print(geneSelectionList)
+            print("May be")
+
+            geneSelectionResult = result_data[14]
+            rows = geneSelectionResult.strip().split('\n')
+
+            # Extract the header and remove extra spaces
+            header = rows[0].split()
+            header = [col.strip() for col in header]
+
+            # Initialize an empty list to store the data
+            result = []
+
+            # Loop through the rows starting from the second row (to skip the header)
+            for row in rows[1:]:
+                # Split the row by spaces
+                values = row.split()
+                # Remove extra spaces from each value
+                values = [val.strip() for val in values]
+                # Append the values as a list to the result list
+                result.append(values)
+
+            # Pass the modified DataFrame to the template
+            return render_template('ranking.html', columns=columns, 
+                                rows=result[:], tNames=tNames, uniqueID=uniqueID,
+                                submissionTime=submissionTime, expirationTime=expirationTime, 
+                                time_taken=time_taken, path=path,
+                                measure=measure, common_genes=common_genes, 
+                                common_sample=common_sample, query_genes=query_genes, 
+                                no_common_sample=no_common_sample, no_common_gene=no_common_gene, hgnc_code=hgnc_code)
+        else:
+            print("yes")
+            return render_template('ranking.html', columns=columns, 
+                                rows=result_list[:], tNames=tNames, uniqueID=uniqueID,
                                 submissionTime=submissionTime, expirationTime=expirationTime, 
                                 time_taken=time_taken, path=path,
                                 measure=measure, common_genes=common_genes, 
@@ -474,8 +601,7 @@ def cleanup_expired_results():
 @app.route('/get_centrality', methods=['POST', 'GET'])
 @profile
 def get_centrality():
-    
-    
+
     if request.method == 'POST':
         
         start_time = time.time()
@@ -488,14 +614,43 @@ def get_centrality():
         uniqueID = request.form.get('uniqueID')
         tNames = request.form.get('tNames')
         
-        store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, 'Null')
+        #store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, 'Null')
         email = request.form.get('email')
+
+        print("Kartik")
+
+        geneSelection = None if not request.form.get('geneData') else request.form.get('geneData')
         
+
+        print("---------------")
+        print(geneSelection)
+        print("------------------")
+
+
+
+        if geneSelection:  # Check if geneSelection is not empty or None
+
+            try:
+                print(1)
+                # Try to convert gene_data to an integer
+                geneSelectionK = int(geneSelection)
+                # If successful, use the integer version
+                #print("Received gene data as integer:", geneSelectionK)
+                store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, 'Null', geneSelectionK, 'Null')
+            except ValueError:
+                print(2)
+                # If error in converting to int, treat as string
+                geneSelectionList = geneSelection
+                #print("Received gene data as string:", geneSelectionList)
+                store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, 'Null', 'Null', geneSelectionList)
+        else:
+                print(3)
+                store_initial_entry(uniqueID, tNames, submissionTime, expirationTime, measure, 'Null', 'Null', 'Null')
+
+                
 
         tissues = json.loads(request.form.get('tissues'))
 
-        #print("Emergency")
-        #print(tissues)
         # tissues = sorted(tissues, key=lambda t: t['name'])
                 # Store the tissue names with unique IDs and expiration times
 
@@ -534,46 +689,29 @@ def get_centrality():
 
             send_email(email, 'Multicens', message)
 
-        result = pd.DataFrame()
-        #print(1)        
+        result = pd.DataFrame()      
         try:
             folder_path = os.path.join('static/sampleData', uniqueID)
             os.makedirs(folder_path, exist_ok=True)
             files = request.files.getlist('files')
             for file in files:
-                #print(2)
                 if file and allowed_file(file.filename):
-                    #print("2a")
                     filename = secure_filename(file.filename)
                     file_path = os.path.join(folder_path, filename)
-                    file.save(file_path)
-            #print("3")        
+                    file.save(file_path)      
             for tissue in tissues:
-                #print("3b")
                 if tissue['dataset'] == 'GTex' :
-                    #print("3aa")
                     tissue_name = tissue_names[tissue['name'].lower()]
                     tissue['exp'] = os.path.join(gtex_path, f'{tissue_name}.v8.normalized_expression.bed.gz')
                     tissue['cov'] = os.path.join(gtex_cov_path,f'{tissue_name}.v8.covariates.txt')
                 elif tissue['dataset'] == 'MSBB' :
-                    #print("3ab")
                     tissue_name = tissue_names[tissue['name'].lower()]
-                    #print(tissue_name)
                     tissue['exp'] = os.path.join(msbb_path, f'{tissue_name}.bed.gz')
-                    #print("GK")
                 else:
-                    #print("3ab")
                     tissue['exp'] = os.path.join(folder_path, tissue['file'])
-            #print("You")
-            #print(tissues)
             A, gene_count = create_corr_matrix(uniqueID, tissues)
-            #print("Hii")
-            #print(A)
-            #print("got")
             n = len(A)/len(tissues)
-            #print("4")
-            if measure == 'query':
-                #print("4a")              
+            if measure == 'query':            
                 query_tissue = request.form.get('query-tissue')
                 query_file = request.files.get('query-file')
                 query_index = int(request.form.get('tissue-index'))                
@@ -587,17 +725,13 @@ def get_centrality():
                 _, g = right_target_global_centrality_t(A.values, num_layers=len(tissues), target_tissue = query_index, target_gene_indices = genes_indices, start=s, end=e, p=0.9)
                 result['Centrality'] = g
             elif measure == 'global':
-                #print("4b")
                 result['Centrality'] = global_centrality(A.values, len(tissues), p=0.9)
-            else:
-                #print("4c")               
+            else:              
                 result['Centrality'] = local_centrality(A.values, len(tissues), p=0.9)
             name_list = [[tissues[i]['name'] for _ in range(gene_count[i])] for i in range(len(tissues))]
             result['Tissue Name'] =  list(itertools.chain(*name_list))
             result['Gene Name'] = [re.sub(r'\.\d+$', '', s) for s in A.columns]
-            #print("5")
             if measure == 'local':
-                #print("5a")
                 ranking = pd.DataFrame()
                 for tissue in tissues:
                     df = result.loc[result['Tissue Name']==tissue['name']]
@@ -606,15 +740,31 @@ def get_centrality():
                     ranking = pd.concat([ranking, df], ignore_index=True)
                 result = ranking
             else:
-                #print("5b")
                 result = result.sort_values(by='Centrality', ascending=False)
                 result['Rank'] = [i+1 for i in range(len(result))]
             result = result[['Tissue Name', 'Gene Name', 'Centrality', 'Rank']]
-            #print("5c")
         except Exception as e:
             return jsonify({"error": str(e)}), 500
         
-        # print(result)
+
+        geneSelectionResult = pd.DataFrame()
+
+        if geneSelectionList:
+            # Convert geneSelectionList string to a list of gene names
+            gene_selection_list = [gene.strip() for gene in geneSelectionList.split(',')]
+
+            # Filter result DataFrame based on intersection of gene names
+            gene_selection_result = result[result['Gene Name'].isin(gene_selection_list)].copy()
+
+            # Reset index to start rank from 1
+            gene_selection_result.reset_index(drop=True, inplace=True)
+
+            # Assign new ranks starting from 1
+            gene_selection_result['Rank'] = gene_selection_result.index + 1
+
+            geneSelectionResult = gene_selection_result.to_string(index=False)
+
+            
         result_csv = os.path.join('static/sampleData', uniqueID + '.csv')    
         result.to_csv(result_csv)
         result['Centrality'] = np.round(result['Centrality'], 4)
@@ -625,9 +775,8 @@ def get_centrality():
         # time_taken = f'{execution_time} {unit}'
         time_taken = f'{execution_time} min(s)'
         print("Execution time:", time_taken, "\n")
-        #print(result_csv)
 
-        update_result_in_db(uniqueID, result, time_taken, result_csv)
+        update_result_in_db(uniqueID, result, time_taken, result_csv, geneSelectionResult)
 
         if email:
             table = pd.DataFrame({
@@ -682,4 +831,3 @@ def get_centrality():
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
